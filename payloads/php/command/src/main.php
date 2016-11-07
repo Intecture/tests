01a7948b74b2ea5c3f3bfe3aebcf8f0cd1b1cdfc
Pete Hayes <pete@hayes.id.au>
2016-11-07 16:11:02 +0000

    Fix bug referencing data item

--- payloads/php/command/src/main.php
+++ payloads/php/command/src/main.php
@@ -11,11 +11,9 @@ if ($argc < 2) {
     exit(1);
 }
 
-echo 'Connecting to host...';
 $host = Host::connect_payload($argv[1], $argv[2]);
-echo 'done', PHP_EOL;
 
-$whoami = new Command($host->data()['cmd']);
+$whoami = new Command($host->data()['command']['cmd']);
 $result = $whoami->exec($host);
 assert($result['exit_code'] == 0);
 assert($result['stdout'] == 'root');
